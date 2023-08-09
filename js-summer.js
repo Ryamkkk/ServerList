7596f03e62db60c260fc9b636799c92c1e9a651a
Ryamkkk <84320037+Ryamkkk@users.noreply.github.com>
2023-08-09 22:23:59 +0300

    Update js-summer.js

--- js-summer.js
+++ js-summer.js
@@ -6,20 +6,34 @@ const startButton = document.querySelector('#startButton');
 
 // Функция для обновления прогресс бара
 function updateProgressBar(value) {
-  progressBar.setAttribute('aria-valuenow', value);
   progressBar.style.width = `${value}%`;
+  progressBar.setAttribute('aria-valuenow', value);
+
+  if (value < 30) {
+    progressBar.classList.remove('bg-success', 'bg-warning');
+    progressBar.classList.add('bg-danger');
+  } else if (value < 70) {
+    progressBar.classList.remove('bg-success', 'bg-danger');
+    progressBar.classList.add('bg-warning');
+  } else {
+    progressBar.classList.remove('bg-warning', 'bg-danger');
+    progressBar.classList.add('bg-success');
+  }
+}
+
+// Функция для запуска изменения прогресса
+function startProgressChange() {
+  let value = 0; // начальное значение прогресса
+
+  const interval = setInterval(() => {
+    if (value <= 100) {
+      updateProgressBar(value);
+      value += 10; // увеличиваем значение прогресса
+    } else {
+      clearInterval(interval); // останавливаем интервал после достижения 100%
+    }
+  }, 1000); // интервал в миллисекундах (здесь 1000 мс = 1 секунда)
 }
 
 // Событие нажатия на кнопку
-startButton.addEventListener('click', () => {
-  let progressValue = 0; // начальное значение прогресса
-
-  const interval = setInterval(() => {
-    if (progressValue < 100) {
-      progressValue += 10; // увеличиваем значение на 10 (может быть другой шаг)
-      updateProgressBar(progressValue);
-    } else {
-      clearInterval(interval); // останавливаем интервал, когда достигли 100%
-    }
-  }, 1000); // интервал в миллисекундах (здесь 1000 мс = 1 секунда)
-});
+startButton.addEventListener('click', startProgressChange);
