26982916b5fe4e7cacb0ec4637620a01f655a51b
Ryamkkk <84320037+Ryamkkk@users.noreply.github.com>
2023-08-09 22:21:08 +0300

    Update js-summer.js

--- js-summer.js
+++ js-summer.js
@@ -1,26 +1,25 @@
 // Получаем элемент прогресс бара
 const progressBar = document.querySelector('.progress-bar');
 
-// Пример значения, которое приходит в запросе (ваша логика может быть другой)
-const progressValue = 46; // Здесь 50 - это просто пример значения
+// Получаем кнопку, которая будет запускать изменение прогресса
+const startButton = document.querySelector('#startButton');
 
-// Обновляем значение атрибута "aria-valuenow" для прогресс бара
-progressBar.setAttribute('aria-valuenow', progressValue);
-
-// Обновляем ширину прогресс бара на основе полученного значения
-progressBar.style.width = `${progressValue}%`;
-
-// Если необходимо, можно также обновить текст внутри прогресс бара
-// progressBar.textContent = `${progressValue}%`;
-
-// Меняем классы прогресс бара в зависимости от значения (это просто пример)
-if (progressValue < 30) {
-  progressBar.classList.remove('bg-success', 'bg-warning');
-  progressBar.classList.add('bg-danger');
-} else if (progressValue < 70) {
-  progressBar.classList.remove('bg-success', 'bg-danger');
-  progressBar.classList.add('bg-warning');
-} else {
-  progressBar.classList.remove('bg-warning', 'bg-danger');
-  progressBar.classList.add('bg-success');
+// Функция для обновления прогресс бара
+function updateProgressBar(value) {
+  progressBar.setAttribute('aria-valuenow', value);
+  progressBar.style.width = `${value}%`;
 }
+
+// Событие нажатия на кнопку
+startButton.addEventListener('click', () => {
+  let progressValue = 0; // начальное значение прогресса
+
+  const interval = setInterval(() => {
+    if (progressValue < 100) {
+      progressValue += 10; // увеличиваем значение на 10 (может быть другой шаг)
+      updateProgressBar(progressValue);
+    } else {
+      clearInterval(interval); // останавливаем интервал, когда достигли 100%
+    }
+  }, 1000); // интервал в миллисекундах (здесь 1000 мс = 1 секунда)
+});
